7f4f51acb6e704e6f07b9c1f0eaa719a348dd1cb
Ivan Derlich <a@ivanderlich.com>
2024-05-30 01:46:52 -0300

    First commit

---
+++ app/news/page.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function NewsPage() {
+  return (
+    <>
+      <h1>News Page</h1>
+      <ul>
+        <li>
+          <a href="news/1">News 1</a>
+        </li>
+        <li>
+          <a href="news/2">News 2</a>
+        </li>
+        <li>
+          <a href="news/3">News 3</a>
+        </li>
+      </ul>
+    </>
+  );
+}
+
+export default NewsPage;
