b80e32a1ff69c4ae4500efe739d3111778e11c85
Ivan Derlich <a@ivanderlich.com>
2024-06-03 02:29:01 -0300

    Add archive by month

--- app/archive/@archive/[[...filter]]/page.tsx
+++ app/archive/@archive/[[...filter]]/page.tsx
@@ -1,24 +1,66 @@
-// import NewsList from "@/components/NewsList";
-// import { getNewsForYear } from "@/helper-methods/news";
-import { getAvailableNewsYears } from "@/helper-methods/news";
+import NewsList from "@/components/NewsList";
+import {
+  getAvailableNewsYears,
+  getNewsForYear,
+  getNewsForYearAndMonth,
+  getAvailableNewsMonths,
+} from "@/helper-methods/news";
 import Link from "next/link";
 
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
 function page({ params: { filter } }) {
-  // const news = getNewsForYear(filter);
-  const links = getAvailableNewsYears();
+  const selectedYear = filter?.[0];
+  const selectedMonth = filter?.[1];
+  let news = [];
+  let links = getAvailableNewsYears();
+  let newsContent = <p>No news fond for the selected period</p>;
+  if (selectedYear && !selectedMonth) {
+    news = getNewsForYear(selectedYear);
+    links = getAvailableNewsMonths(selectedYear);
+  } else if (selectedYear && selectedMonth) {
+    news = getNewsForYearAndMonth(selectedYear, selectedMonth);
+    links = [];
+  }
+
+  if (news && news.length > 0) {
+    newsContent = <NewsList news={news} />;
+  }
+
   console.log("filter: ", filter);
   return (
-    <header id="archive-header">
-      <nav>
-        <ul>
-          {links.map((link) => (
-            <li key={link}>
-              <Link href={`/archive/${link}`}>{link}</Link>
-            </li>
-          ))}
-        </ul>
-      </nav>
-    </header>
+    <>
+      <header id="archive-header">
+        <nav>
+          <ul>
+            {links.map((link) => {
+              const href_ext = selectedYear ? `${selectedYear}/${link}` : link;
+              return (
+                <li key={link}>
+                  <Link href={`/archive/${href_ext}`}>
+                    {selectedYear ? months[link] : link}
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+      </header>
+      {newsContent}
+    </>
   );
 }
 
