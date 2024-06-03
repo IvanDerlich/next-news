703b516b891f05210051df9f284979d81c007559
Ivan Derlich <a@ivanderlich.com>
2024-06-03 14:49:22 -0300

    Single Image Page

--- app/news/[slug]/page.tsx
+++ app/news/[slug]/page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { DUMMY_NEWS } from "@/dummy-news";
 import DisplayImage from "@/components/DisplayImage";
 import { notFound } from "next/navigation";
+import Link from "next/link";
 
 function NewsArticlePage({ params: { slug } }) {
   const newsItem = DUMMY_NEWS.find((newsItem) => newsItem.id === slug);
@@ -13,11 +14,13 @@ function NewsArticlePage({ params: { slug } }) {
   return (
     <article className="new-article">
       <header>
-        <DisplayImage
-          src={`/images/news/${newsItem.image}`}
-          alt={newsItem.title}
-          ratio={6 / 10}
-        />
+        <Link href={`/news/${slug}/picture`}>
+          <DisplayImage
+            src={`/images/news/${newsItem.image}`}
+            alt={newsItem.title}
+            ratio={6 / 10}
+          />
+        </Link>
         <h1>{newsItem.title}</h1>
         <time dateTime={newsItem.date}>{newsItem.date}</time>
       </header>
--- app/news/[slug]/picture/page.tsx
+++ app/news/[slug]/picture/page.tsx
@@ -1,3 +1,20 @@
-export default function page() {
-  return <div className="fullscreen">page</div>;
+import { notFound } from "next/navigation";
+import { DUMMY_NEWS } from "@/dummy-news";
+import DisplayImage from "@/components/DisplayImage";
+export default function page({ params: { slug } }) {
+  console.log("slug: ", slug);
+  const newsItem = DUMMY_NEWS.find((newsItem) => newsItem.id === slug);
+
+  if (!newsItem) {
+    notFound();
+  }
+  return (
+    <div className="fullscreen">
+      <DisplayImage
+        src={`/images/news/${newsItem.image}`}
+        alt={newsItem.title}
+        ratio={7 / 10}
+      />
+    </div>
+  );
 }
