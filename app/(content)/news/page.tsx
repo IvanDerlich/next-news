4cdd41fa0d456ff2d61e39070470c51dfcc1e0fc
Ivan Derlich <a@ivanderlich.com>
2024-07-17 16:12:35 -0300

    Server side fetching

--- app/(content)/news/page.tsx
+++ app/(content)/news/page.tsx
@@ -1,34 +1,9 @@
 "use client";
 import NewsList from "@/components/NewsList";
-import { useEffect, useState } from "react";
 
-function NewsPage() {
-  const [error, setError] = useState("");
-  const [isLoading, setIsLoading] = useState(true);
-  const [news, setNews] = useState([]);
-
-  useEffect(() => {
-    async function fetchNews() {
-      const response = await fetch("http://localhost:8080/news");
-      if (!response.ok) {
-        setError("Failed to fetch news.");
-      }
-      const news = await response.json();
-      console.log(news);
-      setIsLoading(false);
-      setNews(news);
-    }
-    fetchNews();
-  }, []);
-
-  if (isLoading) {
-    return <p>Loading...</p>;
-  }
-
-  if (error) {
-    // to do: Use default next page for errors
-    return <p>{error}</p>;
-  }
+async function NewsPage() {
+  const response = await fetch("http://localhost:8080/news");
+  const news = await response.json();
 
   return (
     <>
