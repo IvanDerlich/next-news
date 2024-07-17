c9ae10461c0a2dd29581120cf167e17cadde8230
Ivan Derlich <a@ivanderlich.com>
2024-07-17 18:02:34 -0300

    Simplify code and add error cases

--- app/(content)/news/page.tsx
+++ app/(content)/news/page.tsx
@@ -3,12 +3,15 @@ import NewsList from "@/components/NewsList";
 
 async function NewsPage() {
   const response = await fetch("http://localhost:8080/news");
+  if (!response.ok) {
+    throw new Error("Failed to fetch news.");
+  }
   const news = await response.json();
 
   return (
     <>
       <h1>News Page</h1>
-      {news ? <NewsList news={news} /> : <p>No news found.</p>}
+      <NewsList news={news} />
     </>
   );
 }
