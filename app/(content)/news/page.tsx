ce2a739b601d17769ef8d55d4d1bce330d049358
Ivan Derlich <a@ivanderlich.com>
2024-07-17 13:21:10 -0300

    Add client side fetching of news from a backend service

--- app/(content)/news/page.tsx
+++ app/(content)/news/page.tsx
@@ -1,11 +1,39 @@
-import { DUMMY_NEWS } from "@/dummy-news";
+"use client";
 import NewsList from "@/components/NewsList";
+import { useEffect, useState } from "react";
 
 function NewsPage() {
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+  const [news, setNews] = useState([]);
+
+  useEffect(() => {
+    async function fetchNews() {
+      const response = await fetch("http://localhost:8080/news");
+      if (!response.ok) {
+        setError("Failed to fetch news.");
+      }
+      const news = await response.json();
+      console.log(news);
+      setIsLoading(false);
+      setNews(news);
+    }
+    fetchNews();
+  }, []);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    // to do: Use default next page for errors
+    return <p>{error}</p>;
+  }
+
   return (
     <>
       <h1>News Page</h1>
-      <NewsList news={DUMMY_NEWS} />
+      {news ? <NewsList news={news} /> : <p>No news found.</p>}
     </>
   );
 }
